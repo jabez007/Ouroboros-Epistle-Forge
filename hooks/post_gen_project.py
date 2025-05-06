08502eafdd145e979632545a792192cfb229adf4
jabez007 <jabez007@users.noreply.github.com>
2025-05-06 12:52:12 -0500

    nitpicks from PR

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -1,5 +1,20 @@
 # Called after project is generated
 
+import os
+
+REMOVE_PATHS = [
+    '{% if cookiecutter.use_poetry == "yes" %} requirements.txt {% endif %}',
+    '{% if cookiecutter.use_poetry == "no" %} pyproject.toml {% endif %}',
+]
+
+for path in REMOVE_PATHS:
+    path = path.strip()
+    if path and os.path.exists(path):
+        if os.path.isdir(path):
+            os.rmdir(path)
+        else:
+            os.unlink(path)
+
 print(
     "\n🎉 Project generated successfully! Run 'poetry install' or 'pip install -r requirements.txt' to get started."
 )
